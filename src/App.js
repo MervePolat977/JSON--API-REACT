cdd9ae36dbde751f895bec113ee2afe8923e7015
Merve Polat <75504698+MervePolat977@users.noreply.github.com>
2023-03-12 14:11:50 +0300

    Add comments to App.js related on async componentDidMount funtion

--- src/App.js
+++ src/App.js
@@ -19,6 +19,21 @@ class App extends Component {
   /* bileşenin ilk kez yüklendiğinde çağrılan bir React yaşam döngüsü yöntemidir.
    Bu yöntem, JSON verilerini almak için bir HTTP isteği yapar ve sonucunu 
    state nesnesine ekler.*/
+  /*Bu örnekte, bileşen monte edildiğinde, fetch fonksiyonu kullanılarak "https://jsonplaceholder.typicode.com/users" 
+  adresine bir istek gönderilir. Bu adres, JSON verisi içeren bir API sunucusudur. fetch fonksiyonu, asenkron bir işlemdir 
+  ve sonucun elde edilmesi için beklememize neden olabilir.
+
+  await anahtar kelimesi, bir Promise nesnesinin sonucunu beklemek için kullanılır. Bu durumda, fetch fonksiyonu bir Promise döndürür 
+  ve await anahtar kelimesi sonucun hazır olmasını bekleyecektir.
+
+  response.json() yöntemi, yanıtın JSON verilerini okumak için kullanılır. Bu yöntem de asenkron bir işlemdir ve 
+  sonucun hazır olması için beklememiz gerekir.
+
+  JSON verileri, data değişkenine atanır ve setState yöntemi kullanılarak users ve loading bileşen durumları
+  güncellenir. users bileşen durumu, API'den alınan kullanıcı verilerini içerirken, loading bileşen durumu, 
+  verilerin yüklenme durumunu belirlemek için kullanılır.
+
+ Böylece, componentDidMount yöntemi, bileşen monte edildiğinde, API'den verileri yükler ve bileşen durumlarını günceller.*/
   async componentDidMount() {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await response.json();
